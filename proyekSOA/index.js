77b91d5cd02f70ba227f621af489a48229a56f59
Joshualauw <65075138+Joshualauw@users.noreply.github.com>
2022-05-23 17:05:58 +0700

    Add files via upload

---
+++ proyekSOA/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require("./joshua/joshua.routes"));
+app.use(require("./ivan/ivan.routes"));
+app.use(require("./erico/erico.routes"));
+app.use(require("./stephen/stephen.routes"));
+
+app.listen(3000, () => {
+  console.log("server connected");
+});
