a53e0d835eebd06fc68060360037fcf1f8402d0f
Joshualauw <65075138+Joshualauw@users.noreply.github.com>
2022-06-23 11:13:51 +0700

    limit watchlist for free users

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 app.use(require("./joshua/joshua.routes"));
-app.use(require("./ivan/ivan.routes"));
+// app.use(require("./ivan/ivan.routes"));
 app.use(require("./erico/erico.routes"));
 //app.use(require("./stephen/stephen.routes"));
 
